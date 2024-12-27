46c1e8aaf95e735af8c43c35c19e3d8f594fbc79
Christian Lapinig <clapinig@Christians-MacBook-Pro.local>
2024-12-27 12:14:42 -0800

    add frontend and backend services to compose.dev.yml

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 // import { useState } from 'react'
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
 import './App.css'
 
 function App() {
   return (
-    <>
+    <Provider theme={defaultTheme}>
       <h1>Integer to Roman numeral service</h1>
-    </>
+    </Provider>
   )
 }
 
