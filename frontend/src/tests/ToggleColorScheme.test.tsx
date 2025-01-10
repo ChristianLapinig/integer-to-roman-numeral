c47661cc64a57aa3727a9632915e5ff8d6c788cb
Christian Lapinig <clapinig@adobe.com>
2025-01-09 16:37:08 -0800

    add frontend tests

---
+++ frontend/src/tests/ToggleColorScheme.test.tsx
@@ -0,0 +1,32 @@
+
+import { describe, it, expect  } from "vitest";
+import { fireEvent, render, RenderResult } from "@testing-library/react";
+
+import App from "../App";
+
+describe("Test ToggleColorScheme", () => {
+	let app: RenderResult;
+
+	beforeEach(() => {
+		app = render(<App />);
+		app.debug();
+	});
+	
+	describe("toggle color scheme", () => {
+		it("shows light theme by default", () => {
+			expect(document.body.className).toBe("light");
+			const { getByTestId } = app;
+			const toggleSwitch = getByTestId("theme-toggle");
+			expect(toggleSwitch.checked).toBe(false);
+		});
+
+		it("toggles color scheme after clicking toggle switch", () => {
+			const { getByTestId } = app;
+			const toggleSwitch = getByTestId("theme-toggle");
+
+			fireEvent.click(toggleSwitch);
+			expect(toggleSwitch.checked).toBe(true);
+			expect(document.body.className).toBe("dark");
+		});
+	});
+});
